933684a1be6b495ab3ab2e9a998984df192df9ac
AkshayGojiya <152589405+AkshayGojiya@users.noreply.github.com>
2025-01-14 22:51:22 +0530

    created first file

--- Mathematics/countDigits.py
+++ Mathematics/countDigits.py
@@ -1,1 +1,14 @@
-print("Hello, world!")
+def countDigs(n):
+    count = 0
+    while n!= 0:
+        n = n // 10
+        count += 1
+    return count
+
+if __name__ == '__main__':
+    n = int(input("Enter the number to count the digits : "))
+
+    a = countDigs(n)
+    print("Number of digits in the given number :", a)
+
+    
