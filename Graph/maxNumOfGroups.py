9720635f42314feddef9a75c280f0b88748042e0
AkshayGojiya <152589405+AkshayGojiya@users.noreply.github.com>
2025-01-30 15:42:35 +0530

    adding the code for divide nodes into the maximum number of groups

---
+++ Graph/maxNumOfGroups.py
@@ -0,0 +1,33 @@
+from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def magnificentSets(self, n: int, edges: List[List[int]]) -> int:
+        g = [[] for _ in range(n)]
+        for a, b in edges:
+            g[a - 1].append(b - 1)
+            g[b - 1].append(a - 1)
+        d = defaultdict(int)
+        for i in range(n):
+            q = deque([i])
+            dist = [0] * n
+            dist[i] = mx = 1
+            root = i
+            while q:
+                a = q.popleft()
+                root = min(root, a)
+                for b in g[a]:
+                    if dist[b] == 0:
+                        dist[b] = dist[a] + 1
+                        mx = max(mx, dist[b])
+                        q.append(b)
+                    elif abs(dist[b] - dist[a]) != 1:
+                        return -1
+            d[root] = max(d[root], mx)
+        return sum(d.values())
+    
+if __name__ == '__main__':
+    obj = Solution()
+    n = 6   
+    edges = [[1,2],[1,4],[1,5],[2,6],[2,3],[4,6]]
+    print(obj.magnificentSets(n, edges))
